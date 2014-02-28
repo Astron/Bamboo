88bc53519a668c20b5116b383e6a27161dc5e91a
kestred <kestred@riotcave.com>
2014-02-28 14:23:54 -0700

    Add byte-ordering to traits.

---
+++ src/traits/traits.cpp
@@ -0,0 +1,21 @@
+// Filename: traits.cpp
+#include "sizetag.h"
+#include "byteorder.h"
+namespace bamboo { // open namespace bamboo
+
+
+// Compile sizetag width into binary
+#ifdef DCLASS_32BIT_SIZETAG
+const int kSizetagWidth = 32;
+#else
+const int kSizetagWidth = 16;
+#endif
+// Compile endianness into binary
+#ifdef PLATFORM_BIG_ENDIAN
+const bool kBigEndian = true;
+#else
+const bool kBigEndian = false;
+#endif
+
+
+} // close namespace bamboo
