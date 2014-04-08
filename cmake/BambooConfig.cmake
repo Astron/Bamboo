46dcf9958cbb78ad2c5f297ce504df627ad88505
kestred <kestred@riotcave.com>
2014-04-08 16:37:54 -0600

    CMake: Add BambooConfig.cmake files.
    This allows other projects using cmake to call find_package(Bamboo[32]) if bamboo was installed to the system.

---
+++ cmake/BambooConfig.cmake
@@ -0,0 +1,16 @@
+# - Config file for the bamboo package
+# It defines the following variables
+#  BAMBOO_INCLUDE_DIRS - include directories for bamboo
+#  BAMBOO_LIBRARIES    - libraries to link against
+
+# Compute paths
+get_filename_component(BAMBOO_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(BAMBOO_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/bamboo")
+
+# Our library dependencies (contains definitions for IMPORTED targets)
+if(NOT TARGET bamboo AND NOT BAMBOO_BINARY_DIR)
+  include("${BAMBOO_CMAKE_DIR}/BambooTargets.cmake")
+endif()
+
+# These are IMPORTED targets created by BambooTargets.cmake
+set(BAMBOO_LIBRARIES bamboo)
