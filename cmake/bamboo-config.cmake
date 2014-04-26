20af6161c271ce00ce198132993be7d6a8be01b4
kestred <kestred@riotcave.com>
2014-04-26 07:08:09 -0600

    Bugfix: Cleanup bamboo-config.cmake and bamboo-targets.cmake to reference installed library.

--- cmake/bamboo-config.cmake
+++ cmake/bamboo-config.cmake
@@ -1,15 +1,17 @@
 # - Config file for the bamboo package
 # It defines the following variables
 #  BAMBOO_INCLUDE_DIRS - include directories for bamboo
+#  BAMBOO_LIBRARY_DIRS - library directories for bamboo
 #  BAMBOO_LIBRARIES    - libraries to link against
 
 # Compute paths
 get_filename_component(BAMBOO_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 set(BAMBOO_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/bamboo")
+set(BAMBOO_LIBRARY_DIRS ${BAMBOO_CMAKE_DIR})
 
 # Our library dependencies (contains definitions for IMPORTED targets)
 if(NOT TARGET bamboo AND NOT BAMBOO_BINARY_DIR)
-  include("${BAMBOO_CMAKE_DIR}/BambooTargets.cmake")
+  include("${BAMBOO_CMAKE_DIR}/bamboo-targets.cmake")
 endif()
 
 # These are IMPORTED targets created by BambooTargets.cmake
--- cmake/bamboo32-config.cmake
+++ cmake/bamboo32-config.cmake
@@ -1,15 +1,17 @@
 # - Config file for the bamboo package
 # It defines the following variables
 #  BAMBOO32_INCLUDE_DIRS - include directories for bamboo
+#  BAMBOO32_LIBRARY_DIRS - library directories for bamboo
 #  BAMBOO32_LIBRARIES    - libraries to link against
 
 # Compute paths
 get_filename_component(BAMBOO32_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-set(BAMBOO32_INCLUDE_DIRS "@CONF_INCLUDE_DIRS@")
+set(BAMBOO32_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/bamboo32")
+set(BAMBOO32_LIBRARY_DIRS ${BAMBOO32_CMAKE_DIR})
 
 # Our library dependencies (contains definitions for IMPORTED targets)
 if(NOT TARGET bamboo32 AND NOT BAMBOO32_BINARY_DIR)
-  include("${BAMBOO32_CMAKE_DIR}/BambooTargets.cmake")
+  include("${BAMBOO32_CMAKE_DIR}/bamboo32-targets.cmake")
 endif()
 
 # These are IMPORTED targets created by Bamboo32Targets.cmake
