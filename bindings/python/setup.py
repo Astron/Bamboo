eb51e20d5519e472ed45a2fc53c6963705494620
Kestred <kestred@riotcave.com>
2014-09-11 23:51:37 -0700

    Improve setup.py with more package information.

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -13,9 +13,18 @@ module = Extension('bamboo',
     libraries = ['bamboo'],
     library_dirs = libraryDirs)
 module.extra_compile_args = ['--std=c++11']
-setup(name='bamboo', version='0.0',
-      description='Bamboo is a library for defining object-oriented message protocols.',
-      author='kestred',
-      author_email='kestred@riotcave.com',
+setup(name = 'bamboo', version = '0.0', license = 'BSD',
+      description = 'Bamboo is a library for defining object-oriented message protocols.',
       url = 'https://github.com/Astron/Bamboo',
-      ext_modules=[module])
+      ext_modules=[module],
+      author = 'kestred',
+      author_email = 'kestred@riotcave.com',
+      classifiers = [
+        'Programming Language :: C++',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ])
